085f940f5e77acc208acfa919b7478f0989a38a7
Saisneha Ravipati <98233363+snehaAshok1@users.noreply.github.com>
2022-08-10 00:24:38 +0530

    initial commit

---
+++ src/main/java/com/sneha/practise/InitialSpring/InitialSpringApplication.java
@@ -0,0 +1,14 @@
+package com.sneha.practise.InitialSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialSpringApplication.class, args);
+		System.out.println("Hello Spring");
+
+	}
+}
